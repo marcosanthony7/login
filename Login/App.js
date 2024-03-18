733ec8e0870758c131a6f3df547d97bc540b429c
Marcos Anthony <130773144+marcosanthony7@users.noreply.github.com>
2024-03-18 08:01:07 -0300

    Add Login app

---
+++ Login/App.js
@@ -0,0 +1,69 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Button, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          borderWidth: 2, // Borda do container
+          borderColor: 'gray', // Cor da borda do container
+          borderRadius: 20, // Arredondamento da borda do container
+          padding: 20, // Espaço interno entre a borda do container e seus filhos
+          backgroundColor: 'white', // Cor de fundo do container
+          width: '80%', // Largura do container
+          alignItems: 'center', // Alinha os filhos ao centro (horizontalmente)
+          gap: 20, // Espaço entre os filhos do container
+          shadowColor: '#333333',
+          shadowRadius: 40,
+        }}>
+        <Text style={[{ fontSize: 30 }, { fontWeight: 'bold' }]}>LOGIN</Text>
+        <TextInput
+          style={[
+            { borderWidth: 2 },
+            { borderRadius: 10 },
+            { padding: 5 },
+            { paddingLeft: 10 },
+            { width: '100%' }, // Ajuste para ocupar toda a largura do container
+          ]}
+          placeholder="Nome"
+        />
+        <TextInput
+          style={[
+            { borderWidth: 2 },
+            { borderRadius: 10 },
+            { padding: 5 },
+            { paddingLeft: 10 },
+            { width: '100%' }, // Ajuste para ocupar toda a largura do container
+          ]}
+          placeholder="Email"
+        />
+        <TextInput
+          style={[
+            { borderWidth: 2 },
+            { borderRadius: 10 },
+            { padding: 5 },
+            { paddingLeft: 10 },
+            { width: '100%' }, // Ajuste para ocupar toda a largura do container
+          ]}
+          placeholder="Senha"
+          secureTextEntry={true}
+        />
+        <View style={{ width: '100%' }}>
+          <Button title="Login" color="blue" />
+        </View>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00bfff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+});
